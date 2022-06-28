5987c0438a85f850104d6167e80403fbb839d102
hyerimme <woncca92@gmail.com>
2022-06-28 22:33:41 +0900

    build_pyramids_done

--- Week_6/build_pyramids.js
+++ Week_6/build_pyramids.js
@@ -1,7 +1,7 @@
 // Here is a skeleton code for you
-let input = 2; // user's input for which pyramid to build
+let input = 3; // user's input for which pyramid to build
 let level = 8; // user's input for how tall the pyramid will be
-process.stdout.write(" ");
+process.stdout.write("");
 /* ToDo: write codes for building pyramids by using for loops (nested)
  and if statement */
 if (input == 1) { // first pyramid
@@ -19,7 +19,26 @@ for (let a = 1; a<=level; a++) {
 }
 
 else if (input == 3) { // third pyramid
-    
-} else { // fourth pyramid
+  for (let a = 1; a<=level; a++) {
+    for (let i=0; i<=level-a; i++) {
+      process.stdout.write(" ");
+    }
+     for (let k=0; k<a; k++) {
+      process.stdout.write("*")
+     }
+    console.log("");
+  }    
+
+} 
+
+else { for (let a = 1; a<=level; a++) {
+  for (let i=0; i<a; i++) {
+    process.stdout.write(" ");
+  }
+   for (let k=0; k<=level-a; k++) {
+    process.stdout.write("*")
+   }
+  console.log("");
+}// fourth pyramid
     
 }
--- Week_6/trying.js
+++ Week_6/trying.js
@@ -1,7 +1,10 @@
 let level =8
 for (let a = 1; a<=level; a++) {
-    for (let i=0; i<=level -a; i++) {
-      process.stdout.write("*");
+    for (let i=0; i<a; i++) {
+      process.stdout.write(" ");
     }
+     for (let k=0; k<=level-a; k++) {
+      process.stdout.write("*")
+     }
     console.log("");
   }
