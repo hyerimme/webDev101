59707e619f6542f0bf735a35b397fa02bcaf20e1
hyerimme <woncca92@gmail.com>
2022-07-09 11:32:22 +0900

    sonan

---
+++ sonan/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='https://s3.amazonaws.com/assets.fullstack.io/n/20200617173023934_start.png' alt='temp' />
+    </div>
+  );
+}
+
+export default App;
