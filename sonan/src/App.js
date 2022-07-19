a5b111d870e4e02d6e36175e1cce144733c11afa
hyerimme <woncca92@gmail.com>
2022-07-19 18:52:55 +0900

    done

--- sonan/src/App.js
+++ sonan/src/App.js
@@ -1,10 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src='https://s3.amazonaws.com/assets.fullstack.io/n/20200617173023934_start.png' alt='temp' />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
