93a6552afdbe8c0e50fece388fb438ca34016d7d
hyerimme <woncca92@gmail.com>
2022-07-29 12:56:23 +0900

    molla

--- sonan/src/components/Sonan_body/Sonan_body.js
+++ sonan/src/components/Sonan_body/Sonan_body.js
@@ -4,6 +4,8 @@ import './Sonan_body.css'
 
 export default function Sonan_body() {
     return (
-    <div className='Sonan_body'>Sonan_body</div>
+    <div className='Sonan_body'> 작고 따뜻한, 소난입니다. </div>
+    
+    
     )
 }
