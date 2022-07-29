a4bc3496e94a0ad4bc1446788b93ca91a86d6b76
hyerimme <woncca92@gmail.com>
2022-07-29 17:50:53 +0900

    molla

--- sonan/src/components/Images/Images.js
+++ sonan/src/components/Images/Images.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import './Images.css'
 import { Routes, Route } from 'react-router-dom'
+import Sonan_body from '../Sonan_body/Sonan_body'
 
 export default function Images() {
     return (
 
         <div className="wrapper">
             <div className="product">
-                <Routes>
-                    <Route path="/" eliment={<Images />} />
+                {/* <Routes> */}
+                    {/* <Route path="/Sonan_body" eliment={<Sonan_body />} /> */}
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/1%20.png?raw=true"></img>
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/2%20.png?raw=true"></img>
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/3.jpeg?raw=true" ></img>
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/4%20.png?raw=true"></img>
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/5.jpeg?raw=true"></img>
                     <img src="https://github.com/hyerimme/webDev101/blob/main/Week_7/6.png?raw=true"></img>
-                </Routes>
+                {/* </Routes> */}
             </div>
         </div>
     )
--- sonan/src/components/Sonan_body/Sonan_body.js
+++ sonan/src/components/Sonan_body/Sonan_body.js
@@ -4,8 +4,11 @@ import './Sonan_body.css'
 
 export default function Sonan_body() {
     return (
-    <div className='Sonan_body'> 작고 따뜻한, 소난입니다. </div>
-    
-    
+    <div className='Sonan_body'> 
+    <h1>작고 따뜻한, 손 안의 무언가들. 소난.</h1>
+    <ul className='explain'>
+        <li> make something small and warm </li>
+    </ul>
+    </div>
     )
 }
